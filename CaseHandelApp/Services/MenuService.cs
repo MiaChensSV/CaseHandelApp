dd5c68f7ed9389acb12a5ed20bb0430df5d1abbe
MiaChensSV <miachensv@gmail.com>
2023-03-23 09:37:18 +0100

    clean up non-inuse Using System parts

--- CaseHandelApp/Services/MenuService.cs
+++ CaseHandelApp/Services/MenuService.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using CaseHandelApp.Models.Entities;
-using CaseHandelApp.Models.Form;
+﻿using CaseHandelApp.Models.Form;
 
 namespace CaseHandelApp.Services
 {
--- CaseHandelApp/Services/UserService.cs
+++ CaseHandelApp/Services/UserService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CaseHandelApp.Contexts;
+﻿using CaseHandelApp.Contexts;
 using CaseHandelApp.Models.Entities;
 using CaseHandelApp.Models.Form;
 using Microsoft.EntityFrameworkCore;
